ceeda50410cc54097ce1d6ab8132422a994d2971
Thiago Thomaz <thiagorthomaz@gmail.com>
2025-01-31 16:52:32 -0300

    migrations

---
+++ FeederDotNet/Migrations/20250131194413_DatasetPKFixTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FeederDotNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatasetPKFixTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Dataset",
+                table: "Dataset");
+
+            migrationBuilder.DropColumn(
+                name: "Uri",
+                table: "Dataset");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "Dataset",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Dataset",
+                table: "Dataset",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Dataset",
+                table: "Dataset");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Dataset");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Uri",
+                table: "Dataset",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Dataset",
+                table: "Dataset",
+                column: "Uri");
+        }
+    }
+}
---
+++ FeederDotNet/Migrations/20250131194705_DatasetAddedUrlField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FeederDotNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatasetAddedUrlField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Dataset",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Dataset");
+        }
+    }
+}
